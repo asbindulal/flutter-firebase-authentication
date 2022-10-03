8cac5fccae361907f8bf0ec03079bdfdab88d743
asbindulal <dulalashi@gmail.com>
2022-10-04 04:26:04 +0545

    update

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -12,68 +12,70 @@ class LoginPage extends StatelessWidget {
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Container(
-        margin: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Hello \nThere!',
-              textScaleFactor: 2.5,
-            ),
-            const SizedBox(
-              height: 35,
-            ),
-            Form(
-              child: Column(
-                children: [
-                  textFormField(
-                    controller: email,
-                    labletext: 'Email',
-                    obscureText: false,
-                    textInputAction: TextInputAction.next,
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  textFormField(
-                    controller: password,
-                    labletext: 'Password',
-                    obscureText: true,
-                    textInputAction: TextInputAction.done,
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  materialButton(
-                    onPressed: () {
-                      userLogin(
-                        context,
-                        email: email.text,
-                        password: password.text,
-                      );
-                    },
-                    text: 'Login',
-                  ),
-                ],
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Hello \nThere!',
+                textScaleFactor: 2.5,
               ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Row(
-              children: [
-                const Text("Don't have an Account?"),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed('/register');
-                  },
-                  child: const Text("Register!"),
-                )
-              ],
-            )
-          ],
+              const SizedBox(
+                height: 35,
+              ),
+              Form(
+                child: Column(
+                  children: [
+                    textFormField(
+                      controller: email,
+                      labletext: 'Email',
+                      obscureText: false,
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    textFormField(
+                      controller: password,
+                      labletext: 'Password',
+                      obscureText: true,
+                      textInputAction: TextInputAction.done,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    materialButton(
+                      onPressed: () {
+                        userLogin(
+                          context,
+                          email: email.text,
+                          password: password.text,
+                        );
+                      },
+                      text: 'Login',
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: [
+                  const Text("Don't have an Account?"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('/register');
+                    },
+                    child: const Text("Register!"),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
